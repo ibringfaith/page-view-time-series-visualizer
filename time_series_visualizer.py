fb72dc42fdc73d66632c07555c6e9b9289b9eb32
Faith Best <97316553+ibringfaith@users.noreply.github.com>
2024-12-21 23:26:50 +0000

    Add index_col parameter

--- time_series_visualizer.py
+++ time_series_visualizer.py
@@ -5,7 +5,9 @@ from pandas.plotting import register_matplotlib_converters
 register_matplotlib_converters()
 
 # Import data (Make sure to parse dates. Consider setting index column to 'date'.)
-df = pd.read_csv('fcc-forum-pageviews.csv', index_col=True, parse_dates=True)
+df = pd.read_csv('fcc-forum-pageviews.csv', index_col='date', parse_dates=True)
+
+print(df.head())
 
 # Clean data
 df = None
